ee3c6ca8d6b33fda21186ce58d59e116f4cd5bc7
Owen-Ross <rossow@sheridancollege.ca>
2022-12-01 13:27:56 -0500

    Updated the layout of UserBid component

--- src/components/UserBid.js
+++ src/components/UserBid.js
@@ -13,25 +13,32 @@ function UserBid(props) {
         <div className="userbid--container">
             <h3 className="userbid--title">Place your bid</h3>
             <form onSubmit={(event) => props.handleBid(event)}>
-                <h4 className="userbid--quantity-title">Quantity</h4>
-                <select 
-                    id="quantity"
-                    value={props.currentQuantity}
-                    onChange={props.handleQuantity}
-                    name="quantity"
-                >
-                    {quantityOptions}
-                </select>
+                <div className="userbid--selection-container">
+                <div className="userbid--selection-container">
+                    <h4 className="userbid--quantity-title">Quantity</h4>
+                    <select 
+                        id="quantity"
+                        value={props.currentQuantity}
+                        onChange={props.handleQuantity}
+                        name="quantity"
+                    >
+                        {quantityOptions}
+                    </select>
+                </div>
                 <br/>
-                <h4>Dice Face</h4>
-                <select
-                    id="face"
-                    value={props.currentFace}
-                    onChange={props.handleFace}
-                    name="face"
-                >
-                    {faceOptions}
-                </select>
+                <div className="userbid--selection-container">
+                    <h4>Dice Face</h4>
+                    <select
+                        id="face"
+                        value={props.currentFace}
+                        onChange={props.handleFace}
+                        name="face"
+                    >
+                        {faceOptions}
+                    </select>
+                </div>
+                </div>
+                <br/>
                 <button className="userbid--bid-button">Place Bid</button>
             </form>
         </div>
